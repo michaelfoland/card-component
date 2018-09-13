f49773187bc0da30d6374d73f9de2ea226cd1604
Michael Foland <michaelfoland@gmail.com>
2018-09-13 17:06:22 -0400

    Add basic styling for card-frame

--- src/CardFrame.js
+++ src/CardFrame.js
@@ -8,6 +8,34 @@ class CardFrame extends HTMLElement {
   connectedCallback() {
     this._root.innerHTML = 
 `
+<style>
+:host {
+  --theme-color: #111;
+  --header-font-color: #fff;
+  display: block;
+  border: solid 2px var(--theme-color);
+  background: var(--theme-color);
+  border-radius: 1em;
+  overflow: hidden;
+  font-family: sans-serif;
+  color: var(--header-font-color);
+}
+.header {
+  margin: 0;
+  background: inherit;
+  color: var(--header-font-color);
+  padding: .5em;
+}
+.content {
+  background: white;
+  color: var(--theme-color);
+}
+.content-frame {
+  padding: 1em;
+  background: white;
+}
+</style>
+
 <h2 class="header"><slot name="header">Card Header</slot></h2>
 <div class="content-frame">
   <div class="visibility-manager">
